8633a61ffa54f9574677f01dd22cd714c3e79c48
skybird <152402712+skybird-r@users.noreply.github.com>
2025-12-06 14:24:53 +0900

    高尾山

--- src/main/java/com/skybird/create_jp_signal/mixin/ScheduleRuntimeMixin.java
+++ src/main/java/com/skybird/create_jp_signal/mixin/ScheduleRuntimeMixin.java
@@ -9,6 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import com.simibubi.create.content.trains.entity.Train;
 import com.simibubi.create.content.trains.graph.DiscoveredPath;
+import com.simibubi.create.content.trains.schedule.Schedule;
 import com.simibubi.create.content.trains.schedule.ScheduleEntry;
 import com.simibubi.create.content.trains.schedule.ScheduleRuntime;
 import com.simibubi.create.content.trains.schedule.ScheduleRuntime.State;
@@ -23,19 +24,20 @@ public abstract class ScheduleRuntimeMixin {
     @Shadow Train train;
     @Shadow public State state;
     @Shadow public int currentEntry;
+    @Shadow Schedule schedule;
     
     @Inject(
         method = "startCurrentInstruction",
-        at = @At("TAIL"),
-        cancellable = true,
-        locals = LocalCapture.CAPTURE_FAILHARD
+        at = @At("HEAD"),
+        cancellable = true
     )
     private void create_jp_signal_onStartCurrentInstructionEnd(
-        CallbackInfoReturnable<DiscoveredPath> cir,
-        ScheduleEntry entry,
-		ScheduleInstruction instruction
+        CallbackInfoReturnable<DiscoveredPath> cir
 
     ) {
+        ScheduleEntry entry = schedule.entries.get(currentEntry);
+		ScheduleInstruction instruction = entry.instruction;
+
         if (instruction instanceof OperationTypeInstruction operation) {
 			((ITrain)train).setOperationType(operation.getOperationType());
 			state = State.PRE_TRANSIT;
